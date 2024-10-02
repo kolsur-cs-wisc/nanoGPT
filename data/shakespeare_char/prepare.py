550ad1ffb5815f823a14799d69515a93b5e39db7
Aditya Kolsur <kolsur@wisc.edu>
2024-10-02 15:37:58 -0500

    Calculate training probability distribution

--- data/shakespeare_char/prepare.py
+++ data/shakespeare_char/prepare.py
@@ -39,11 +39,16 @@ n = len(data)
 train_data = data[:int(n*0.9)]
 val_data = data[int(n*0.9):]
 
+train_data_probs = []
+for char in chars:
+    train_data_probs.append((train_data.count(char))/len(train_data))
+
 # encode both to integers
 train_ids = encode(train_data)
 val_ids = encode(val_data)
 print(f"train has {len(train_ids):,} tokens")
 print(f"val has {len(val_ids):,} tokens")
+print(sum(train_data_probs))
 
 # export to bin files
 train_ids = np.array(train_ids, dtype=np.uint16)
@@ -56,6 +61,8 @@ meta = {
     'vocab_size': vocab_size,
     'itos': itos,
     'stoi': stoi,
+    'train_data_probs': train_data_probs,
+    'chars': chars,
 }
 with open(os.path.join(os.path.dirname(__file__), 'meta.pkl'), 'wb') as f:
     pickle.dump(meta, f)
