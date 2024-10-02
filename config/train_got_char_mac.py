a24ced05d3b261ac09b6909ed725c63f6c775308
Aditya Kolsur <kolsur@wisc.edu>
2024-10-02 14:12:30 -0500

    Outputs and Custom Dataset

--- config/train_got_char_mac.py
+++ config/train_got_char_mac.py
@@ -3,7 +3,7 @@
 
 out_dir = 'out-got-char-cpu'
 eval_interval = 250 # keep frequent because we'll overfit
-eval_iters = 20
+eval_iters = 80
 log_interval = 10 # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
@@ -15,12 +15,12 @@ wandb_run_name = 'mini-gpt'
 
 dataset = 'got'
 gradient_accumulation_steps = 1
-batch_size = 12
+batch_size = 64
 block_size = 256 # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 4
-n_head = 4
+n_layer = 6
+n_head = 6
 n_embd = 384
 dropout = 0.2
 
